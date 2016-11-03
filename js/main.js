f95aa4106af64d37cf677e6a6ae949fee1e991b9
NeoZhangTCL <ztuochao@uwo.ca>
2016-11-02 21:06:41 -0400

    add css

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
-function initialize(){
-    
+function initMap() {
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: uluru,
+    minZoom: 8
+  });
+
+  map.addListener('bounds_changed', function() {
+  // 3 seconds after the center of the map has changed, pan back to the
+  // marker.
+  window.setTimeout(function() {
+    map.panTo(marker.getPosition());
+  }, 3000);
+});
+
 }
 
-function setupRandomLocations(){
+function setMark(){
+
+}
+
+function search(){
+
+}
+
+function getMapBoundry(){
+
+}
+
+function refreshList(){
 
 }
